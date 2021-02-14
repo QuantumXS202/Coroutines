b572ff0d7f1f3016605c9dcdf7a8fe37067d2b77
Quinn Croese <Quinncroese@gmail.com>
2021-02-14 21:06:32 +0100

    Opdracht1

---
+++ GPR1/Assets/Scripts/Deel1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deel1: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Fade");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Fade()
+    {
+        Debug.Log("Ik start nu de coroutine");
+        yield return new WaitForSeconds(.5f);
+        Debug.Log("ik kan hier wel tegen");
+        yield return new WaitForSeconds(.5f);
+        Debug.Log("ik kan hier niet tegen");
+        yield return new WaitForSeconds(.5f); 
+    }
+}
