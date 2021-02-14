2a5a6358d8a3d4f34ee96d70c3ba448d68eecbd7
Quinn Croese <Quinncroese@gmail.com>
2021-02-14 21:27:18 +0100

    Deel3

---
+++ GPR1/Assets/Scripts/Deel3.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deel3: MonoBehaviour
+{
+    public float ex;
+    public Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown("space"))
+        {
+          StartCoroutine("Fade");
+        }
+    }
+
+    IEnumerator Fade()
+    {
+        for (float ft = ex; ft >= 0; ft -= 0.1f)
+        {
+            Color c = renderer.material.color;
+            c.a = ft;
+            renderer.material.color = c;
+            yield return new WaitForSeconds(.1f);
+        }          
+    }
+}
