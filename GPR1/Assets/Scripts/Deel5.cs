32fc4b17a593b5f42b4908499b55608a884f74eb
Quinn Croese <Quinncroese@gmail.com>
2021-02-28 23:29:10 +0100

    jo

---
+++ GPR1/Assets/Scripts/Deel5.cs
@@ -0,0 +1,80 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deel5 : MonoBehaviour
+{
+    public Vector3 endPos;
+    public Vector3 endScale;
+
+    private Vector3 _startPos;
+    private Vector3 _startScale;
+
+    public float moveTime;
+    private float _percent;
+    private float _easeStep;
+
+    public bool move;
+    public bool grow;
+    public bool easeInQuad;
+    public bool easeOutQuad;
+    public bool easeOutQuart;
+
+    private void Start()
+    {
+
+        _startScale = transform.localScale;
+        _startPos = transform.position;
+        _percent = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(EaseIn());
+        }
+    }
+
+    private IEnumerator EaseIn()
+    {
+        while (_percent < 1)
+        {
+            _percent += Time.deltaTime / moveTime;
+
+            if (easeInQuad)
+            {
+                _easeStep = _percent * _percent;
+            }
+            else if (easeOutQuad)
+            {
+                float sum = 1 - _percent;
+                _easeStep = 1 - sum * sum;
+            }
+            else if (easeOutQuart)
+            {
+                float pow = Mathf.Pow(1 - _percent, 4);
+                _easeStep = 1 - pow;
+            }
+
+            if (move)
+            {
+                Vector3 distance = endPos - _startPos;
+                Vector3 travelDistance = distance * _easeStep;
+                Vector3 newPos = _startPos + travelDistance;
+                transform.position = newPos;
+            }
+
+            if (grow)
+            {
+                Vector3 scale = endScale - _startScale;
+                Vector3 scaleIncrease = scale * _easeStep;
+                Vector3 newScale = _startScale + scaleIncrease;
+                transform.localScale = newScale;
+            }
+
+            yield return null;
+        }
+    }
+}
